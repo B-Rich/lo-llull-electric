a7a3cbd13ca125a3ca49861ab65f7c55152d4819
arturgs <arturgs@gmail.com>
2016-09-20 17:30:29 +0200

    add twitter utils

--- util_twitter.py
+++ util_twitter.py
@@ -1,25 +1,12 @@
 import tweepy
 import twitter_keys
 
-consumer_key, consumer_secret, access_token, access_token_secret = twitter_keys.getKeys()
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+def twit_text(text):
+	consumer_key, consumer_secret, access_token, access_token_secret = twitter_keys.getKeys()
 
-api = tweepy.API(auth)
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
-
-'''
-user = api.get_user('twitter')
-
-
-print(user.screen_name)
-print(user.followers_count)
-for friend in user.friends():
-   print(friend.screen_name)
-'''
-
-api.update_status("Lore Ipsum...")
+	api = tweepy.API(auth)
+	api.update_status(test)
