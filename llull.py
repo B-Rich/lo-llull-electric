3e7fef1a66a646836c8efd46277fe1b57bfc0f5f
Fernando Cucchietti <fercook@gmail.com>
2016-10-17 08:21:34 +0200

    Error handling and clean tweets

--- llull.py
+++ llull.py
@@ -2,9 +2,11 @@
 '''
 
 from __future__ import print_function
+import json
 import numpy as np
 import random
 import sys
+import codecs
 from keras.models import load_model
 import util_twitter
 
@@ -26,6 +28,14 @@ def sample(preds, temperature=1.0):
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas)
 
+def cleanText(original): #Remove @usernames and http links
+    splitted = original.strip().split(" ")
+    together = []
+    for word in splitted:
+        if (word.find("@")<0 and word.find("http")<0):
+            together.append(word)
+    return " ".join(together)
+
 model = load_model('models/clean_655.h5')
 maxlen = model.input_shape[1]
 
@@ -34,16 +44,21 @@ fi=open('following.txt','r')
 for line in fi:
     following.append(line.strip())
 fi.close()
+# Future: util_twitter.get_following("lollullelectric")
 
 user = np.random.choice(following)
 tweets = util_twitter.get_twits(user)
 
 temperatures = [0.2, 0.4, 0.7, 1.0]
-for tweet in tweets:
-    text = tweet.text.encode('utf-8')
+for user in following:
+  tweets = util_twitter.get_twits(user)
+  for tweet in tweets:
+    rawtext = tweet.text.encode('utf-8')
+    text = cleanText(rawtext)
     if len(text)<=maxlen:
         print ("Too short: "+text)
     else:
+      try:
         diversity = np.random.choice(temperatures)
         generated = ''
         start_index = random.randint(0, len(text) - maxlen - 1)
@@ -69,4 +84,7 @@ for tweet in tweets:
             sys.stdout.flush()
         generated += " https://twitter.com/"+user+"/status/"+str(tweet.id)
         util_twitter.twit_text(generated)
-        print("\n\n\n\n")
+        print("\n")
+      except:
+        print ("Had problems with "+rawtext)
+        continue
